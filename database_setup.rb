6321bab1021db8d075c4964519bdc2b675ebe22f
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-17 12:52:53 -0500

    Add photo model. Update database with new table.

--- database_setup.rb
+++ database_setup.rb
@@ -3,3 +3,12 @@ unless ActiveRecord::Base.connection.table_exists?(:photographers)
     t.string :name
   end  
 end
+
+unless ActiveRecord::Base.connection.table_exists?(:photos)
+  ActiveRecord::Base.connection.create_table :photos do |t|
+    t.string :date
+    t.string :title
+    t.string :location
+    t.integer :photographer_id
+  end  
+end
